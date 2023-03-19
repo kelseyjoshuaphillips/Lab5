aad435693c2c6c4f1e7a46b0bdc1465e2426e3da
kelseyjoshuaphillips <phil0382@algonquinlive.com>
2023-03-19 14:55:56 -0400

    Modified create and delete, still need more

---
+++ Lab5/Pages/Predictions/Create.cshtml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Lab5.Data;
+using Lab5.Models;
+using Azure.Storage.Blobs;
+
+namespace Lab5.Pages.Predictions
+{
+    public class CreateModel : PageModel
+    {
+        private readonly BlobServiceClient _blobServiceClient;
+        private readonly string earthContainerName = "earthimages";
+        private readonly string computerContainerName = "computerimages";
+        private readonly Lab5.Data.PredictionDataContext _context;
+
+        public CreateModel(Lab5.Data.PredictionDataContext context, BlobServiceClient blobServiceClient)
+        {
+            _blobServiceClient = blobServiceClient;
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Prediction Prediction { get; set; }
+        
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+          if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Predictions.Add(Prediction);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ Lab5/Pages/Predictions/Delete.cshtml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Lab5.Data;
+using Lab5.Models;
+using Azure.Storage.Blobs;
+
+namespace Lab5.Pages.Predictions
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly BlobServiceClient _blobServiceClient;
+        private readonly string earthContainerName = "earthimages";
+        private readonly string computerContainerName = "computerimages";
+        private readonly Lab5.Data.PredictionDataContext _context;
+
+        public DeleteModel(Lab5.Data.PredictionDataContext context, BlobServiceClient blobServiceClient)
+        {
+            _context = context;
+            _blobServiceClient = blobServiceClient;
+        }
+
+        [BindProperty]
+      public Prediction Prediction { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null || _context.Predictions == null)
+            {
+                return NotFound();
+            }
+
+            var prediction = await _context.Predictions.FirstOrDefaultAsync(m => m.PredictionId == id);
+
+            if (prediction == null)
+            {
+                return NotFound();
+            }
+            else 
+            {
+                Prediction = prediction;
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null || _context.Predictions == null)
+            {
+                return NotFound();
+            }
+            var prediction = await _context.Predictions.FindAsync(id);
+
+            if (prediction != null)
+            {
+                Prediction = prediction;
+                _context.Predictions.Remove(Prediction);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
