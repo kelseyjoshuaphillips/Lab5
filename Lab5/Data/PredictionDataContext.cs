f129c13e11cc3e7804b19b0bf8032230f4b24ae8
kelseyjoshuaphillips <phil0382@algonquinlive.com>
2023-03-19 14:32:25 -0400

    Before migration

---
+++ Lab5/Data/PredictionDataContext.cs
@@ -0,0 +1,16 @@
+﻿using Lab5.Models;
+using Microsoft.EntityFrameworkCore;
+namespace Lab5.Data
+{
+    public class PredictionDataContext : DbContext
+    {
+        public PredictionDataContext (DbContextOptions<PredictionDataContext> options) : base(options) { }
+
+        public DbSet<Prediction> Predictions { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Prediction>().ToTable("prediction");
+            //base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ Lab5/Models/Prediction.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Lab5.Models
+{
+    public enum Question
+    {
+        Earth, Computer
+    }
+    public class Prediction
+    {
+        public int PredictionId { get; set; }
+        [Required]
+        [StringLength(50)]
+        [Column("FileName")]
+        [Display(Name ="File Name")]
+        public string FileName { get; set; }
+        [Required]
+        [StringLength(100)]
+        public string Url { get; set; }
+
+        [Required]
+        public Question Question { get; set; }
+    }
+}
