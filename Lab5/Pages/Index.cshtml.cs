ad7426f288a2516ed40fb425755bb2d443772035
kelseyjoshuaphillips <phil0382@algonquinlive.com>
2023-03-19 14:46:42 -0400

    Before scaffolding

--- Lab5/Pages/Index.cshtml.cs
+++ Lab5/Pages/Index.cshtml.cs
@@ -5,6 +5,12 @@ namespace Lab5.Pages
 {
     public class IndexModel : PageModel
     {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
         public void OnGet()
         {
         }
